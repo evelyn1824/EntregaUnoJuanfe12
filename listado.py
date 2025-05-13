8310c066bd4a503985342fe503fd54de7d3fe200
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-05-13 17:28:05 -0500

    se crea un cambio

--- listado.py
+++ listado.py
@@ -23,6 +23,7 @@
 
 
 #------------------------------------------------
+
 opcion=None
 
 print("TAREAS APP")
@@ -32,19 +33,29 @@ print("3. Modificar una tarea")
 print("4. Eliminar una tarea")
 print("5. SALIR")
 
-
+listaTareas=[]
 while opcion != 5:
+    tarea={}
     opcion=int(input("Digita una opcion por favor: "))
     if opcion == 1:
-        pass
+        print("Creando una tarea...")
+        tarea['id']=input("Digita el id de la tarea: ")
+        tarea['descripcion']=input("Digita una descripcion: ")
+        tarea['diaSemana']=input("¿Qué dias es esta tarea?': ")
+        tarea['hora']=input("¿A que hora es esta tarea?: ")
+        tarea['fecha']=input("Digita la fecha del evento (AAAA-MM-DD)")
+
+    
+        listaTareas.append(tarea)
     elif opcion == 2:
-        pass
+        print("Consultando las tareas...")
+        print(listaTareas)
     elif opcion == 3:
-        pass 
+        print("Modificando una tarea...")
     elif opcion == 4:
-        pass
+        print("Eliminando una tarea...")
     elif opcion == 5:
-        print("saliendo")
+        print("Saliendo")
         break
     else:
         print("Opcion invalida")
